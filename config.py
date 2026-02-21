ad114a67a1e921bc113a8772be9d34979f36c011
KhanMdAyan <KhanMdAyan0@gmail.com>
2026-02-21 15:02:22 +0530

    Project done

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ DB_CONFIG = {
     "password": "1212",
     "database": "library_db"
 }
+
+EMAIL_CONFIG = {
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587,
+    "sender_email": "your_email@gmail.com",
+    "sender_password": "your_app_password"
+}
